00e9e74dc82eeb4219d78108938be1a18f9017f3
Maryna Bakum <2605094+marina8915@users.noreply.github.com>
2017-10-10 19:26:56 +0200

    edited empty element

--- Homework1/index.js
+++ Homework1/index.js
@@ -1,22 +1,18 @@
 // JavaScript Document
 function inputArray() {
-    var array1 = [1,NaN,3,5,-3, ,];
-    //delete empty element
-    var array = [];
-    for (var i = 0; i < array1.length; i++) {
-        if (i in array1) {
-            array.push(array1[i]);
-        }
-    }
+    var array = [1, NaN, 3, 5, -3, ,];
     var Sum = 0;
-	var Max = null;
-	var Min = null;
+    var Max = null;
+    var Min = null;
     for (var i = 0; i < array.length; i++) {
-        array[i] = +array[i]; //transform into numbers
-        if (!isNaN(array[i])) {
-            Sum = Sum + array[i];
-			if (Max < array[i]) Max = array[i]; 
-			if (Min > array[i]) Min = array[i];
+        if (i in array) { //rejection empty element
+            array[i] = +array[i]; //transform into numbers
+
+            if (!isNaN(array[i])) {
+                Sum = Sum + array[i];
+                if (Max < array[i]) Max = array[i];
+                if (Min > array[i]) Min = array[i];
+            }
         }
     }
     //output Array, Sum, Max, Min
