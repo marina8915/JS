ed3d8a8d2bb715907acafdc2bbc0e183af2c318b
Maryna Bakum <2605094+marina8915@users.noreply.github.com>
2017-10-11 23:10:58 +0200

    Update index.js

--- Homework1/index.js
+++ Homework1/index.js
@@ -4,14 +4,14 @@ function inputArray() {
     var Sum = 0;
     //conferment Min, Max
     for (var i = 0; i < array.length; i++) {
-        if (typeof(array[i]) === "number" & !isNaN(array[i])) {
+        if (typeof(array[i]) === "number" && !isNaN(array[i])) {
             var Max = array[i];
             var Min = array[i];
             break;
         }
     }
     for (var i = 0; i < array.length; i++) {
-        if (i in array & typeof(array[i]) === "number" & !isNaN(array[i])) { //rejection empty element, boollean, NaN
+        if (i in array && typeof(array[i]) === "number" && !isNaN(array[i])) { //rejection empty element, boollean, NaN
             Sum = Sum + array[i];
             if (Max < array[i]) Max = array[i];
             if (Min > array[i]) Min = array[i];
