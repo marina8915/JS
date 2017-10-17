8bae4eda5bd45a32ec38afbed24286915061fadd
marina <marina_bakum@mail.ru>
2017-10-17 19:24:16 +0300

    update index.js

--- Homework2/index.js
+++ Homework2/index.js
@@ -10,23 +10,21 @@ while (!(array[maxIndex] === 0)) {
             maxIndex = index;
         }
     });
-    console.log(max);
-    console.log(maxIndex);
     array.forEach(function (el, index) {
-        var bigger;
-        var big;
+        var biggerLeft;
+        var biggerRight;
         if (el === 0) {
             if (el[index] < array[index + 1] && el[index] < array[index - 1]) {
                 water += 1;
             } else {
-                bigger = array.find(function (el, ind) {
+                biggerRight = array.find(function (el, ind) {
                     return (el > 0 && ind < index);
                 });
-                if (bigger > 0) {
-                    big = array.find(function (el, ind) {
+                if (biggerRight > 0) {
+                    biggerLeft = array.find(function (el, ind) {
                         return (el > 0 && ind > index);
                     });
-                    if (big > 0) water += 1;
+                    if (biggerLeft > 0) water += 1;
                 }
             }
         }
