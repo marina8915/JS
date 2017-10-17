11b3b66477bbb615c949437ce8ce34c494ab9f10
marina <marina_bakum@mail.ru>
2017-10-17 19:47:21 +0300

    added comments

--- Homework2/index.js
+++ Homework2/index.js
@@ -4,16 +4,19 @@ var maxIndex = 999;
 var water = 0;
 
 while (!(array[maxIndex] === 0)) {
+    //find max, maxIndex
     array.forEach(function (element, index) {
         if (max < element) {
             max = element;
             maxIndex = index;
         }
     });
+    //find element = 0
     array.forEach(function (el, index) {
         var biggerLeft;
         var biggerRight;
         if (el === 0) {
+            //search for neighbors with left and right > 0
             if (el[index] < array[index + 1] && el[index] < array[index - 1]) {
                 water += 1;
             } else {
@@ -29,6 +32,7 @@ while (!(array[maxIndex] === 0)) {
             }
         }
     });
+    //reduce all elements of the array by 1
     array = array.map(function (el) {
         el = el - 1;
         if (el < 0) el = 0;
