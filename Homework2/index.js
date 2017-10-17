77d40d33136dc46c325c5eac6339ac25369794e4
marina <marina_bakum@mail.ru>
2017-10-17 22:16:20 +0300

    deleted max,maxIndex,comparison with array[index+1], array[index-1]; added sum,

--- Homework2/index.js
+++ Homework2/index.js
@@ -1,34 +1,22 @@
 var array = [2, 1, 5, 0, 3, 4, 7, 2, 3, 1, 0];
-var max = -999;
-var maxIndex = 999;
 var water = 0;
+var sum = 100;
 
-while (!(array[maxIndex] === 0)) {
-    //find max, maxIndex
-    array.forEach(function (element, index) {
-        if (max < element) {
-            max = element;
-            maxIndex = index;
-        }
-    });
+while (sum > 0) {
     //find element = 0
     array.forEach(function (el, index) {
         var biggerLeft;
         var biggerRight;
         if (el === 0) {
             //search for neighbors with left and right > 0
-            if (el[index] < array[index + 1] && el[index] < array[index - 1]) {
-                water += 1;
-            } else {
-                biggerRight = array.find(function (el, ind) {
-                    return (el > 0 && ind < index);
+            biggerRight = array.find(function (el, ind) {
+                return (el > 0 && ind < index);
+            });
+            if (biggerRight > 0) {
+                biggerLeft = array.find(function (el, ind) {
+                    return (el > 0 && ind > index);
                 });
-                if (biggerRight > 0) {
-                    biggerLeft = array.find(function (el, ind) {
-                        return (el > 0 && ind > index);
-                    });
-                    if (biggerLeft > 0) water += 1;
-                }
+                if (biggerLeft > 0) water += 1;
             }
         }
     });
@@ -38,6 +26,10 @@ while (!(array[maxIndex] === 0)) {
         if (el < 0) el = 0;
         return el;
     });
+    //find sum of elements
+    sum = array.reduce(function (sum,el) {
+        return sum + el;
+    },0);
 }
 
 alert(water);
