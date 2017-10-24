47fcb89c46d1fa46fd496ccff623d78537d97512
marina <marina_bakum@mail.ru>
2017-10-24 15:32:09 +0300

    edited methods create, eat; created methods drink, lesson, wash, functions life

--- Homework3/dragonfly.js
+++ Homework3/dragonfly.js
@@ -1,5 +1,3 @@
-var i = 0
-
 function Dragonfly() {
     var appetite
     var health
@@ -7,45 +5,135 @@ function Dragonfly() {
     var thirst
     var cleanliness
     var education
-    var timeToEat
+    var ignorEat = 0
+    var ignoreDrink = 0
+    var ignoreStudy = 0
+    var ignoreWash = 0
+
+    var create
+    var eat
+    var drink
+    var lesson
+    var wash
+    var info
+    var help
+
     this.create = function (name) {
         this.name = name || 'name';
-        console.log('Hello, I am dragonfly, my name is - ' + this.name)
-        appetite = 100
+        console.log('Hello, I am dragonfly, my name is ' + this.name)
+        appetite = 80
         health = 100
-        humor  = 100
+        humor = 80
         thirst = 100
         cleanliness = 100
         education = 0
-        this.timeToEat = setInterval(function () {
-            console.log("Please, Give me the feed")
-            i++
-            if (i > 5) {
-                health = health - 20
-                humor = humor - 50
-                appetite = appetite - 50
-            }
-        }, 2000)
+        life()
     }
     this.eat = function (food) {
         var feed = ['mosquito', 'midge', 'fly']
         var provender = feed.find(function (el) {
             return el === food
         })
-        if (provender) {
-            console.log('Yummy')
-            i = 0
-        } else {
-            console.log('Please, give me the ' + feed)
+        if (appetite === 100) {
+            console.log('Thank you, I don`t want to eat')
+        }else{
+            if (provender) {
+                console.log('Yummy')
+                appetite += 20
+                thirst -= 10
+                humor += 10
+                health += 10
+                ignorEat = 0
+            } else {
+                console.log('Please, give me the ' + feed)
+            }
         }
-        this.appetite = 100
+
+        life()
+
     }
-    this.die = function () {
+    this.drink = function () {
+        if (thirst === 100) console.log('Thank you, I don`t want to drink')
+        console.log('Thank you')
+        thirst += 20
+        humor += 10
+        health += 10
+        cleanliness -= 10
+        ignoreDrink = 0
+        life()
+    }
+    this.lesson = function () {
+        console.log('I like to fly')
+        education += 10
+        appetite -= 20
+        health += 30
+        cleanliness -= 20
+        humor += 20
+        ignoreStudy = 0
+        console.log('I want to wash')
+        ignoreWash++
+        if (ignoreWash > 2) {
+            health -= 10
+            humor -= 50
+            cleanliness -= 20
+        }
+        life()
+    }
+    this.wash = function () {
+        if (thirst === 100) {
+            console.log('Thank you, I don`t want to wash')
+        } else {
+            console.log('Thank you')
+            cleanliness += 20
+            health += 20
+            humor += 20
+            ignoreWash = 0
+        }
+        life()
+    }
+    function life () {
+        if (health <= 0) die()
+        if (humor <= 0 || appetite <= 0 || thirst <= 0 || cleanliness <= 0) {
+            health -= 50
+            humor -= 50
+        }
+        if(appetite < 100) {
+            console.log("Please, give me the feed")
+            ignorEat++
+        }
+        if (ignorEat > 3) {
+            health -= 20
+            humor -= 20
+            appetite -= 10
+        }
+        if(thirst < 100) {
+            console.log("Please, give me the water")
+            ignoreDrink++
+        }
+        if (ignoreDrink > 5) {
+            health -= 20
+            humor -= 20
+            thirst -= 10
+        }
+        if (health < 50 && health !== 0) {
+            console.log("I want to study")
+            ignoreStudy++
+        }
+        if (ignoreStudy > 2) {
+            health -= 10
+            humor -= 10
+        }
+        if (humor < 100) {
+            console.log("I want to study")
+            ignoreStudy++
+        }
+        if (health < 20) setTimeout(die, 20000)
+    }
+    function die() {
         console.log('Farewell :(')
-        delete dragon
-        clearInterval(this.timeToEat)
+        dragon = null
     }
-    this.info = function(){
+    this.info = function () {
         console.table([
             ['health', health],
             ['appetite', appetite],
@@ -55,14 +143,14 @@ function Dragonfly() {
             ['thirst', thirst]
         ])
     }
-    this.help = function(){
+    this.help = function () {
         console.table([
-            ['create(name)', 'the creation of a dragonfly'],
-            ['eat(\'feed\')', 'feed, dragonfly eat mosquito, midge, fly'],
-            ['info()', 'check parameters: health, appetite, humor and other'],
-            ['drink()', 'give to drink water'],
-            ['wash()', 'to wash'],
-            ['lesson()', 'dragonfly is studying to fly']
+            ['dragon.create(\'name\')', 'the creation of a dragonfly'],
+            ['dragon.eat(\'feed\')', 'feed, dragonfly eat mosquito, midge, fly'],
+            ['dragon.info()', 'check parameters: health, appetite, humor and other'],
+            ['dragon.drink()', 'give to drink water'],
+            ['dragon.wash()', 'to wash'],
+            ['dragon.lesson()', 'dragonfly is studying to fly']
         ])
     }
 }
