43ecab8b1538a506099e1ae44e455dcca15c9208
marina <marina_bakum@mail.ru>
2017-10-24 18:51:17 +0300

    myMap

--- Homework3/index.js
+++ Homework3/index.js
@@ -1,5 +1,6 @@
-var array = [1,3]
+var array = [1, 3]
 
+//myForEach
 Array.prototype.myForEach = function (myCell) {
     for (var i = 0; i < this.length; i++) {
         myCell(this[i], i, this)
@@ -7,6 +8,20 @@ Array.prototype.myForEach = function (myCell) {
 
 }
 array.myForEach(function (el) {
-    console.log(el+10)
+    //console.log(el+10)
 
 })
+
+//myMap
+Array.prototype.myMap = function (myCell) {
+    var Map = []
+    for (var i = 0; i < this.length; i++) {
+        Map[i] = myCell(this[i])
+    }
+    return Map
+}
+var arr = array.myMap(function (el) {
+    return el + 10
+})
+
+console.log(arr)
