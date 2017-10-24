e98d57c73c02184544be45cd5b667869651cf353
marina <marina_bakum@mail.ru>
2017-10-24 18:13:18 +0300

    myForEach

--- Homework3/index.js
+++ Homework3/index.js
@@ -1,12 +1,12 @@
 var array = [1,3]
 
-
-
-Array.prototype.myForEach=function(el,index){
-    for (var i = 0; i < array.length; i++) {
-        el = array[i]++
-        index = i
+Array.prototype.myForEach = function (myCell) {
+    for (var i = 0; i < this.length; i++) {
+        myCell(this[i], i, this)
     }
+
 }
-array.myForEach()
- console.log(array.__proto__['get arguments'])
+array.myForEach(function (el) {
+    console.log(el+10)
+
+})
