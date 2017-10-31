36678051b6e06d27aae7ce1f65cf1836a9540075
Maryna Bakum <2605094+marina8915@users.noreply.github.com>
2017-10-31 16:10:56 +0200

    edited mySort

--- Homework3/index.js
+++ Homework3/index.js
@@ -1,10 +1,10 @@
-var array = [1, 210, 100]
+var array = [1, 4, 6, 5, 4, 5, 10, 3, 3]
 var str = ["white", "red", "black"]
 
 //myForEach
-Array.prototype.myForEach = function (myCellback) {
+Array.prototype.myForEach = function (myForEach) {
     for (var i = 0; i < this.length; i++) {
-        myCellback(this[i], i, this)
+        myForEach(this[i], i, this)
     }
 }
 array.myForEach(function (el) {
@@ -12,10 +12,10 @@ array.myForEach(function (el) {
 })
 
 //myMap
-Array.prototype.myMap = function (myCellback) {
+Array.prototype.myMap = function (myMap) {
     var Map = []
     for (var i = 0; i < this.length; i++) {
-        Map[i] = myCellback(this[i])
+        Map[i] = myMap(this[i])
     }
     return Map
 }
@@ -25,26 +25,41 @@ var arr = array.myMap(function (el) {
 //console.log(arr)
 
 //mySort
-Array.prototype.mySort = function (myCellback) {
-    var myCellback = function () {}
+Array.prototype.mySort = function (mySort) {
+    var mySort = mySort || function (a, b) {
+            return a > b
+        }
+    var max
     var Sort = []
-    var Max
     var length = this.length
-    for (var c = 0; c < length; c++) {
-        for (var i = 0; i < length - 1; i++) {
-            if (myCellback(this[c], this[c + 1])) {
-                Max = this[c]
+    for (var i = 0; i < length - 1; i++) {
+        for (var c = 0; c < length - i - 1; c++) {
+            if (mySort(this[c], this[c + 1])) {
+                max = this[c]
                 this[c] = this[c + 1]
-                this[c + 1] = Max
+                this[c + 1] = max
             }
         }
-        Sort[c] = this[c]
     }
-    return Sort
+    return this
 }
-var sort = array.mySort(function (prev, next) {
-    return prev > next
+var sort = array.mySort(function (a, b) {
+    return a > b
 })
 var sortStr = str.mySort()
 console.log(sort)
 console.log(sortStr)
+
+sort = array.mySort(function (a, b) {
+    if (a > b) return 1
+    if (a < b) return -1
+    return 0
+})
+console.log(sort)
+
+sort = array.mySort(function (a, b) {
+    if (a > b) return -1
+    if (a < b) return 1
+    return 0
+})
+console.log(sort)
