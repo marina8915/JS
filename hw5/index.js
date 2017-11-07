640d971f9eefe10460a9affd366910bd86214228
Maryna Bakum <2605094+marina8915@users.noreply.github.com>
2017-11-07 23:42:33 +0200

    fixed click, added checked

--- hw5/index.js
+++ hw5/index.js
@@ -1,9 +1,23 @@
-function  Click() {
-    document.getElementById('submit').addEventListener('click', function (event) {
-        var el = document.createElement("li")
-        el.innerHTML = document.getElementById("todo").value
-       // document.getElementById("list").appendChild(el)
-        list.insertBefore(el, list.childNodes[0])
-        event.stopPropagation()
-    }, false)
-} 
+function onLoadFunct() {
+    var click = document.getElementById('submit')
+    if (click) {
+        click.addEventListener('click', function (event) {
+            event.stopPropagation()
+            var el = document.createElement("li")
+            el.innerHTML = '<input type="checkbox" class="check">' + document.getElementById("todo").value
+            list.insertBefore(el, list.childNodes[0])
+        }, false)
+    }
+
+    var checked = document.getElementById('checked')
+    if (checked) {
+        checked.addEventListener('click', function (event) {
+            event.stopPropagation()
+            Array.prototype.forEach.call(document.getElementsByClassName("check"), function (el) {
+                if (el.checked) {
+                        select.insertBefore(el, select.childNodes[0])
+                }
+            })
+        }, false)
+    }
+}
