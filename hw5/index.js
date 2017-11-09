ef3e2f6b7e9bd3dbc74eceb91b7d47e96220c954
marina <marina_bakum@mail.ru>
2017-11-09 13:33:05 +0200

    added comments, todo->item,unElems->el

--- hw5/index.js
+++ hw5/index.js
@@ -1,22 +1,25 @@
 function onLoadFunct() {
-
+    //event for button submit
     var click = document.getElementById('submit')
-    var n = 0
+    var n = 0 //variable for assign an id
     if (click) {
         click.addEventListener('click', function (event) {
             event.stopPropagation()
             n += 1
+            //create item in list
             var el = document.createElement("li")
             el.id = n
-            var value = -document.getElementById("todo").value
+            //check if input empty -> ignore
+            var value = -document.getElementById("item").value
             if (value !== -0){
-                el.innerHTML = '<input type="checkbox" id="check' + n + '">' + document.getElementById("todo").value
+                el.innerHTML = '<input type="checkbox" id="check' + n + '">' + document.getElementById("item").value
                 list.insertBefore(el, list.childNodes[0])
             }
-            document.getElementById("todo").value = ""
+            //reset input
+            document.getElementById("item").value = ""
         }, false)
     }
-
+    //event for button all
     var all = document.getElementById('all')
     if (all) {
         all.addEventListener('click', function (event) {
@@ -37,11 +40,13 @@ function onLoadFunct() {
             }, false)
         })
     }
+    //event for button checked
     var checked = document.getElementById('checked')
     if (checked) {
         checked.addEventListener('click', function (event) {
             event.stopPropagation()
             Array.prototype.forEach.call(document.getElementsByTagName("li"), function () {
+                //find selected element in list
                 el = document.getElementById('list').getElementsByTagName('li')
                 for (i = 0; i < el.length; i++) {
                     var idLi = el[i].getAttribute('id')
@@ -51,43 +56,46 @@ function onLoadFunct() {
                         document.getElementById("select").appendChild(el[i])
                     }
                 }
-            el = document.getElementById('unselect').getElementsByTagName('li')
-            for (i = 0; i < el.length; i++) {
-                idLi = el[i].getAttribute('id')
-                idCheck = "check" + idLi
-                li = document.getElementById(idCheck)
-                if (li.checked) {
-                    document.getElementById("select").appendChild(el[i])
+                //find selected element in unselect
+                el = document.getElementById('unselect').getElementsByTagName('li')
+                for (i = 0; i < el.length; i++) {
+                    idLi = el[i].getAttribute('id')
+                    idCheck = "check" + idLi
+                    li = document.getElementById(idCheck)
+                    if (li.checked) {
+                        document.getElementById("select").appendChild(el[i])
+                    }
                 }
-            }
-        }, false)
+            }, false)
         })
     }
-
+    //event for button unchecked
     var unchecked = document.getElementById('unchecked')
     if (unchecked) {
         unchecked.addEventListener('click', function (event) {
             event.stopPropagation()
             Array.prototype.forEach.call(document.getElementsByTagName("li"), function () {
-                var unElems = document.getElementById('list').getElementsByTagName('li')
-                for (i = 0; i < unElems.length; i++) {
-                    idLi = unElems[i].getAttribute('id')
+                //find unselected element in list
+                el = document.getElementById('list').getElementsByTagName('li')
+                for (i = 0; i < el.length; i++) {
+                    idLi = el[i].getAttribute('id')
                     idCheck = "check" + idLi
                     li = document.getElementById(idCheck)
                     if (li.checked === false) {
-                        document.getElementById("unselect").appendChild(unElems[i])
+                        document.getElementById("unselect").appendChild(el[i])
                     }
                 }
-            unElems = document.getElementById('select').getElementsByTagName('li')
-            for (i = 0; i < unElems.length; i++) {
-                idLi = unElems[i].getAttribute('id')
-                idCheck = "check" + idLi
-                li = document.getElementById(idCheck)
-                if (li.checked === false) {
-                    document.getElementById("unselect").appendChild(unElems[i])
+                //find unselected element in unselect
+                el = document.getElementById('select').getElementsByTagName('li')
+                for (i = 0; i < el.length; i++) {
+                    idLi = el[i].getAttribute('id')
+                    idCheck = "check" + idLi
+                    li = document.getElementById(idCheck)
+                    if (li.checked === false) {
+                        document.getElementById("unselect").appendChild(el[i])
+                    }
                 }
-            }
-        }, false)
+            }, false)
         })
     }
 }
