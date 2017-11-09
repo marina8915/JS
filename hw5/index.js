beea371738732c662e793c14f1de314abd0de08d
marina <marina_bakum@mail.ru>
2017-11-09 12:41:26 +0200

    fixed checked, unchecked, add all

--- hw5/index.js
+++ hw5/index.js
@@ -14,12 +14,35 @@ function onLoadFunct() {
         }, false)
     }
 
-    //bag not new list
+    var all = document.getElementById('all')
+    if (all) {
+        all.addEventListener('click', function (event) {
+            event.stopPropagation()
+            Array.prototype.forEach.call(document.getElementsByTagName("li"), function (el) {
+                var elems = document.getElementById('select').getElementsByTagName('li')
+                for (var i = 0; i < elems.length; i++) {
+                    document.getElementById("list").appendChild(elems[i])
+                }
+            }, false)
+            Array.prototype.forEach.call(document.getElementsByTagName("li"), function (el) {
+                var elems = document.getElementById('unselect').getElementsByTagName('li')
+                for (var i = 0; i < elems.length; i++) {
+                    document.getElementById("list").appendChild(elems[i])
+                }
+            }, false)
+            Array.prototype.forEach.call(document.getElementsByTagName("li"), function (el) {
+                var elems = document.getElementById('list').getElementsByTagName('li')
+                for (var i = 0; i < elems.length; i++) {
+                    document.getElementById("list").appendChild(elems[i])
+                }
+            }, false)
+        })
+    }
     var checked = document.getElementById('checked')
     if (checked) {
         checked.addEventListener('click', function (event) {
             event.stopPropagation()
-            Array.prototype.forEach.call(document.getElementsByTagName("li"), function (el) {
+            Array.prototype.forEach.call(document.getElementsByTagName("li"), function (li) {
                 var elems = document.getElementById('list').getElementsByTagName('li')
                 for (var i = 0; i < elems.length; i++) {
                     var idLi = elems[i].getAttribute('id')
@@ -29,16 +52,24 @@ function onLoadFunct() {
                         document.getElementById("select").appendChild(elems[i])
                     }
                 }
-            }, false)
+            var elems = document.getElementById('unselect').getElementsByTagName('li')
+            for (var i = 0; i < elems.length; i++) {
+                var idLi = elems[i].getAttribute('id')
+                var idCheck = "check" + idLi
+                var li = document.getElementById(idCheck)
+                if (li.checked) {
+                    document.getElementById("select").appendChild(elems[i])
+                }
+            }
+        }, false)
         })
     }
 
-    //bag not new list
     var unchecked = document.getElementById('unchecked')
     if (unchecked) {
         unchecked.addEventListener('click', function (event) {
             event.stopPropagation()
-            Array.prototype.forEach.call(document.getElementsByTagName("li"), function (el) {
+            Array.prototype.forEach.call(document.getElementsByTagName("li"), function (li) {
                 var unElems = document.getElementById('list').getElementsByTagName('li')
                 for (var i = 0; i < unElems.length; i++) {
                     var idLi = unElems[i].getAttribute('id')
@@ -48,7 +79,16 @@ function onLoadFunct() {
                         document.getElementById("unselect").appendChild(unElems[i])
                     }
                 }
-            }, false)
+            var unElems = document.getElementById('select').getElementsByTagName('li')
+            for (var i = 0; i < unElems.length; i++) {
+                var idLi = unElems[i].getAttribute('id')
+                var idCheck = "check" + idLi
+                var li = document.getElementById(idCheck)
+                if (li.checked === false) {
+                    document.getElementById("unselect").appendChild(unElems[i])
+                }
+            }
+        }, false)
         })
     }
 }
