daac7c597fdd06eec0f065bac162c874b3526b2b
Maryna Bakum <2605094+marina8915@users.noreply.github.com>
2017-11-18 17:08:26 +0200

    changeName,changeStatus

--- hw6.1-2/home_work_6_1.js
+++ hw6.1-2/home_work_6_1.js
@@ -5,13 +5,25 @@ function Client(name, second_name, status) {
     this.name = function () {
         return console.log('Name: ' + userName + ' ' + secondName)
     }
+	 this.changeName = function (name, second_name) {
+		 userName = name
+		 secondName = second_name
+        return console.log('New name: ' + userName + ' ' + secondName)
+    }
     this.status = function () {
         return console.log('Status : ' + userStatus)
+    }
+	this.changeStatus = function (status) {
+		userStatus = status
+        return console.log('New status : ' + userStatus)
     }
 	return this
 }
 
 var client1 = new Client('Lera', 'Briantseva', 'usual')
 client1.name()
-var client2 = new Client('Marina', 'Bakum', 'usual')
-client2.status()
+client1.changeName('Marina', 'Bakum')
+client1.name()
+client1.status()
+client1.changeStatus('special')
+client1.status()
